b95d5c0dc53fe9cc56b911777c05a89a366c333c
Jean Paul Iradukunda <uyuwe60@gmail.com>
2026-02-26 19:34:29 +0200

    Add CI with Java 17

--- src/test/java/playwright_automation/SortingTest.java
+++ src/test/java/playwright_automation/SortingTest.java
@@ -1,7 +1,59 @@
 package playwright_automation;
 
-public class SortingTest {
-    public static void main(String[] args)  {
+import com.microsoft.playwright.*;
+import org.junit.jupiter.api.*;
 
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SortingTest {
+
+    static Playwright playwright;
+    static Browser browser;
+    BrowserContext context;
+    Page page;
+
+    @BeforeAll
+    static void launchBrowser() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+    }
+
+    @BeforeEach
+    void createContextAndPage() {
+        context = browser.newContext();
+        page = context.newPage();
+    }
+
+    @AfterEach
+    void closeContext() {
+        context.close();
+    }
+
+    @AfterAll
+    static void closeBrowser() {
+        browser.close();
+        playwright.close();
+    }
+
+    @Test
+    public void verifyPriceLowToHighSorting() {
+        page.navigate("https://practicesoftwaretesting.com/");
+
+        page.selectOption("select[data-test='sort']", "price,asc");
+        page.waitForTimeout(2000);
+
+        List<String> priceTexts = page.locator(".price").allTextContents();
+
+        List<Double> prices = new ArrayList<>();
+        for (String price : priceTexts) {
+            prices.add(Double.parseDouble(price.replace("$", "").trim()));
+        }
+
+        List<Double> sortedPrices = new ArrayList<>(prices);
+        Collections.sort(sortedPrices);
+
+        assertEquals(sortedPrices, prices, "Products are not sorted correctly!");
     }
 }
